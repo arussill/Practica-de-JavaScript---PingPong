209f7732192679fa8db21de6e6d259ba8ae1277f
Aura Russill <auracris1996@hotmail.com>
2022-04-24 20:23:18 -0500

    ADD - inicio PingPong

---
+++ main.js
@@ -0,0 +1,16 @@
+(function(){
+    var self.Board = function(width, height){
+        this.width = wigth;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+    }
+
+    self.Board.prototype = {
+
+    }
+})();
+function main (){
+
+}
